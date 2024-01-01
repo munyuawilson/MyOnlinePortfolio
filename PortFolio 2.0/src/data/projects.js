e56ec78f3202a2e6370ce5168d187a0202ec1334
munyuawilson <wmunyua4@gmail.com>
2023-12-31 22:21:33 -0500

    update

---
+++ PortFolio 2.0/src/data/projects.js
@@ -0,0 +1,83 @@
+// Import images
+import WebImage1 from '../images/burger.png';
+import WebImage2 from '../images/flower.png';
+import WebImage3 from '../images/pokemon.png';
+import WebImage4 from '../images/RecipeProject.png';
+import WebImage5 from '../images/map.png';
+import MobileImage1 from '../images/space.png';
+import MobileImage2 from '../images/youtube.png';
+import UIImage1 from '../images/gmail.png';
+import UIImage2 from '../images/spotify.png';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'Flower Project',
+		category: 'Web Application',
+		repoUrl:'https://github.com/hasanyalsiz/WebDesign-Flower',
+		img: WebImage2,
+		ProjectHeader: {
+			title: 'Flower Project - From Context',
+			publishDate: 'May 26, 2023',
+			tags: 'React - Frontend',
+		},
+		
+	},
+	{
+		id: 2,
+		title: 'Youtube Clone',
+		category: 'Web Application',
+		img: MobileImage2,
+		repoUrl:'https://github.com/hasanyalsiz/YoutubeClone',
+	},
+	{
+		id: 3,
+		title: 'Gmail Clone',
+		category: 'UI/UX Design',
+		img: UIImage1,
+		repoUrl:'https://github.com/hasanyalsiz/GmailClone',
+	},
+	{
+		id: 4,
+		title: 'Spotify Project',
+		category: 'UI/UX Design',
+		img: UIImage2,
+		repoUrl:'https://github.com/hasanyalsiz/Spotify',
+	},
+	{
+		id: 5,
+		title: 'Space Project',
+		category: 'Web Application',
+		img: MobileImage1,
+		repoUrl:'https://github.com/hasanyalsiz/SpaceProject',
+	},
+	{
+		id: 6,
+		title: 'Burger Project',
+		category: 'Web Application',
+		img: WebImage1,
+		repoUrl:'https://github.com/hasanyalsiz/ReactBurger',
+	},
+	{
+		id: 7,
+		title: 'Pokemon Guide Project',
+		category: 'Web Application',
+		img: WebImage3,
+		repoUrl:'https://github.com/hasanyalsiz/Pokemon',
+	},
+	{
+		id: 8,
+		title: 'Recipe Project',
+		category: 'Web Application',
+		img: WebImage4,
+		repoUrl:'https://github.com/hasanyalsiz/Recipe',
+	},
+	{
+		id: 9,
+		title: 'Map Project',
+		category: 'Web Application',
+		img: WebImage5,
+		repoUrl:'https://github.com/hasanyalsiz/Map',
+	},
+
+];
