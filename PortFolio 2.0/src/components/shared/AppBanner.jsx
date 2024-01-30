c3b36aaa9ff64b7f9a8d4ccb86f1dc5aae346bc4
munyuawilson <wmunyua4@gmail.com>
2024-01-30 23:20:54 +0300

    update

--- PortFolio 2.0/src/components/shared/AppBanner.jsx
+++ PortFolio 2.0/src/components/shared/AppBanner.jsx
@@ -2,6 +2,7 @@ import useThemeSwitcher from '../../hooks/useThemeSwitcher';
 import { FiArrowDownCircle } from 'react-icons/fi';
 import developerLight from '../../images/developer.svg';
 import developerDark from '../../images/developer-dark.svg';
+import Resume from  '../files/WilsonmunyuaResume.pdf';
 import { motion } from 'framer-motion';
 import Typical from 'react-typical'
 import React from 'react';
@@ -76,7 +77,7 @@ const AppBanner = () => {
 						download="WilsonmunyuaResume.pdf"
 						rel='noreferrer'
 						target='_blank'
-						href="/files/WilsonmunyuaResume.pdf"
+						href={Resume}
 						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
 						aria-label="Download Resume"
 					>
