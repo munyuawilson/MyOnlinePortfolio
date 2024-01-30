018807a8fefa3b8e5750dbb0133676f137ee2f28
munyuawilson <wmunyua4@gmail.com>
2024-01-30 21:29:12 +0300

    Links

--- PortFolio 2.0/src/components/shared/AppFooterCopyright.jsx
+++ PortFolio 2.0/src/components/shared/AppFooterCopyright.jsx
@@ -4,7 +4,7 @@ function AppFooterCopyright() {
 			<div className="text-lg text-ternary-dark dark:text-ternary-light">
 				&copy; {new Date().getFullYear()}
 				<a
-					href="https://github.com/hasanyalsiz"
+					href="https://www.linkedin.com/in/wilson-munyua-4a0417204/"
 					target="__blank"
 					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
@@ -12,7 +12,7 @@ function AppFooterCopyright() {
 				</a>
 				.
 				<a
-					href="https://www.linkedin.com/in/hasanyalsiz/"
+					href="https://www.linkedin.com/in/wilson-munyua-4a0417204/"
 					target="__blank"
 					className="text-secondary-dark dark:text-secondary-light font-medium uppercase hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
 				>
